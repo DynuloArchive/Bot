c138373e05659d78150d12194d8159bd308ac7bb
Brett Mayson <brett@bmandesigns.com>
2018-08-30 10:12:37 -0600

    Channel locking. closes #12

--- base/admin/__init__.py
+++ base/admin/__init__.py
@@ -12,3 +12,25 @@ class Admin(bot.Extension):
         """Says something in the specified channel"""
         await ctx.args.channel.send(ctx.args.text)
         await message.add_reaction("✅")
+
+    @bot.role("moderator")
+    @bot.argument("channel*", discord.TextChannel, "here")
+    @bot.command()
+    async def lock(ctx, message):
+        """Locks the specified or current channel"""
+        everyone = discord.utils.find(lambda m: m.name.lower() == "@everyone", message.channel.guild.roles)
+        perms = ctx.args.channel.overwrites_for(everyone)
+        perms.send_messages = False
+        await ctx.args.channel.set_permissions(everyone, overwrite=perms)
+        await message.add_reaction("🔒")
+
+    @bot.role("moderator")
+    @bot.argument("channel*", discord.TextChannel, "here")
+    @bot.command()
+    async def unlock(ctx, message):
+        """Unlocks the specified or current channel"""
+        everyone = discord.utils.find(lambda m: m.name.lower() == "@everyone", message.channel.guild.roles)
+        perms = ctx.args.channel.overwrites_for(everyone)
+        perms.send_messages = None
+        await ctx.args.channel.set_permissions(everyone, overwrite=perms)
+        await message.add_reaction("🔓")
