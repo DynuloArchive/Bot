fdb556d37cfc72e0ec77b989333a185baa88b8f5
Brett <brett@bmandesigns.com>
2018-10-15 02:12:13 -0600

    better counting

--- base/polls/__init__.py
+++ base/polls/__init__.py
@@ -47,6 +47,7 @@ class Polls(bot.Extension):
     async def tally(ctx, message):
         ballots = {}
         spoiled = []
+        alloptions = []
         msg = await message.channel.get_message(int(ctx.args.message))
         await message.add_reaction("✅")
         async for option in message.channel.history(after=msg, limit=5):
@@ -56,10 +57,13 @@ class Polls(bot.Extension):
                         return True
                 return False
             if check("1\u20e3",option):
+                alloptions.append(option.id)
                 logger.debug(f"Counting {option.content}")
                 for reaction in option.reactions:
                     async for user in reaction.users():
                         if user.id == ctx.bot.user.id:
+                            continue
+                        if user in spoiled:
                             continue
                         if user in ballots:
                             if reaction.emoji in ballots[user]:
@@ -75,38 +79,56 @@ class Polls(bot.Extension):
 
         eliminated = []
         solved = False
+        roundnum = -1
         while not solved:
+            roundnum += 1
             round = {}
             total = 0
-            logger.debug("Computing a round")
+            logger.debug(f"Computing round {roundnum}")
             for user in ballots:
                 if user in spoiled:
+                    logger.debug(f"Ignore spolied ballot from {user.display_name}")
                     continue
                 choices = ballots[user]
+                counted = 0
                 for index in ["1\u20e3","2\u20e3","3\u20e3","4\u20e3","5\u20e3"]:
                     if index not in ballots[user]:
+                        logger.debug(f"Ballot from {user.display_name} does not contain index")
                         continue
                     if ballots[user][index] in eliminated:
+                        logger.debug(f"Ballot from {user.display_name} is eliminated")
                         continue
+                    logger.debug(f"Counting ballot from {user.display_name}")
                     total += 1
                     if ballots[user][index] in round:
                         round[ballots[user][index]] += 1
                     else:
                         round[ballots[user][index]] = 1
-                    break
+                    counted += 1
+                    if counted == 2:
+                        break
             for option in round:
-                if round[option] > total / 2:
+                logger.debug(f"{option} had {round[option]} votes out of {(total + 1) / 2}")
+                if round[option] > (total + 1) / 2:
                     solved = True
                     winner = await message.channel.get_message(option)
                     await message.channel.send(f"Winner is {winner.content}")
                     return
             lowest = total
             elim = None
-            for option in round:
+            for option in alloptions:
+                if option not in round and option not in eliminated:
+                    elim = option
+                    lowest = 0
+                    break
+                if option not in round:
+                    continue
                 if round[option] < lowest:
                     elim = option
                     lowest = round[option]
             if elim != None:
+                removed = await message.channel.get_message(elim)
+                logger.error(f"Eliminating {removed.content}")
                 eliminated.append(elim)
             else:
                 await message.channel.send("Inconclusive 🙅")
